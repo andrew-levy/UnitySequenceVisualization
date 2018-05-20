26a31992115acd5ab8037c06cfb964f1d5303587
andrew-levy <alevy97@gmail.com>
2018-05-20 14:14:29 -0400

    Visualizing {1/n}

---
+++ MathTesting/Assets/Scripts/Sequence.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sequence : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+        transform.position = new Vector3(0, 0, 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < 10; i++)
+        {
+            transform.position = new Vector3(0, 0, -i * 10);
+        }
+	}
+}
---
+++ MathTesting/Assets/Scripts/moveSequence.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class moveSequence : MonoBehaviour {
+
+    public Material ironMan;
+
+    public static int count = 1;
+ 
+    public Text countText;
+
+    public GameObject epsilonBall;
+    public float radiusCount = 1;
+
+	private void Start()
+	{
+        
+        epsilonBall.SetActive(false);
+        epsilonBall.transform.localScale = new Vector3(radiusCount,1,1);
+
+        SetText(); 
+	}
+
+	void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+
+
+            count++;
+
+            GameObject newSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            newSphere.transform.localScale = new Vector3((float).3, (float).3, (float).3);
+            newSphere.transform.position = new Vector3 (0, 0, (float)-10 / (float)count);
+
+            newSphere.GetComponent<Renderer>().material = ironMan;
+
+            SetText();
+
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+           
+            radiusCount = (float)radiusCount * 2;
+            epsilonBall.SetActive(true);
+            epsilonBall.transform.localScale = new Vector3(1, radiusCount, 1);
+
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            
+            radiusCount = (float) radiusCount / 2;
+            epsilonBall.SetActive(true);
+            epsilonBall.transform.localScale = new Vector3(1, radiusCount, 1);
+        }
+
+    }
+
+    void SetText()
+    {
+        countText.text = "Term: 1/" + count.ToString();
+    }
+}
